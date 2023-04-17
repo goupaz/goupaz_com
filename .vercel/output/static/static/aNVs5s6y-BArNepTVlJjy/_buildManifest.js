ffee32c1bdc176797dd3e40228b1f22b03efc587
Sako M <sakom@Sakos-MacBook-Pro.local>
2023-04-17 07:04:04 -0700

    u

--- .vercel/output/static/static/aNVs5s6y-BArNepTVlJjy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/555-1ac1e199ceb0c38a.js","static/css/a6086129adba8575.css","static/chunks/pages/index-a9ee552611d1e494.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
